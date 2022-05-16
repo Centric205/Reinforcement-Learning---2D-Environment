3af31728387519b2184afb64e27c89b06fa0cfbc
Theo Madikgetla <theocentric95@gmail.com>
2022-05-16 08:35:46 +0200

    Added the training code for my agent
    This portion of code uses Q-learning to learn about the environment.
    After the last epoch, the agent's trajectory will be displayed.

--- Scenario1.py
+++ Scenario1.py
@@ -14,7 +14,38 @@ def main():
     fourRoomsObj = FourRooms('simple')
     actSeq = [FourRooms.UP,FourRooms.RIGHT, FourRooms.DOWN, FourRooms.LEFT]
 
+    # Hyper-parameters
+    epsilon = 0.1
+    discount_factor = 0.6
+    learning_rate = 0.1
 
+    for i in range(10000):
+        fourRoomsObj.newEpoch()
+        x, y = fourRoomsObj.getPosition()
+        state = x * 13 + y
+
+        isTerminal = False    
+
+        while not isTerminal:
+            if np.random.uniform(0, 1) < epsilon:
+                action = actSeq[np.random.randint(4)]
+            else:
+                action = np.argmax(Q_s_a[state])
+
+            cell_type, (new_X,new_Y), numpack, isTerminal = fourRoomsObj.takeAction(action)
+
+            old_q_value = Q_s_a[state, action]
+            newstate = new_X*13 + new_Y
+            nextmax = np.max(Q_s_a[newstate])
+
+            TP = rewards(cell_type) + (discount_factor * nextmax) - old_q_value
+            newValue = old_q_value + (learning_rate * TP)
+            Q_s_a[state, action] = newValue
+
+            state = newstate
+
+    print(Q_s_a)
+    print("Training finished on epoch: ", i)
     fourRoomsObj.showPath(-1)
 
 
